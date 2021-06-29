1d7a14ad4e2ead1f1161d886ee46917c4612126d
wl <kcwl558106@gmail.com>
2021-06-29 17:29:43 +0800

    c15-4

--- Game.kt
+++ Game.kt
@@ -1,22 +1,45 @@
-/*c13-12
+/*c15-4
 */
 fun main() {
 //呼叫 Player 類別的主建構函數，產生一個類別實體
-    var player = Player("wl")
-    player.castFireBall(5)
-
-    var currentRoom = TownSquare() //Room("Foyer")
+//    var player = Player("wl")
+//    player.castFireBall(5)
+//
+//    var currentRoom = TownSquare() //Room("Foyer")
     println(currentRoom.description())
     println(currentRoom.load())
 
     player.auraColor()
     printPlayerStatus(player)
+    Game.play()
 }
 
-private fun printPlayerStatus(player:Player) {
-    println(
-        "光環顏色：${player.auraColor()}" + "       走運嗎？" +
-                "${if (player.isBlessed) "是的" else "否"}"
-    )
-    println("${player.name}${player.formaHealthStatus()}")
+//private fun printPlayerStatus(player:Player) {
+//    println(
+//        "光環顏色：${player.auraColor()}" + "       走運嗎？" +
+//                "${if (player.isBlessed) "是的" else "否"}"
+//    )
+//    println("${player.name}${player.formaHealthStatus()}")
+//}
+object  Game{
+    private var player = Player("wl")
+    private var currentRoom = TownSquare()
+
+    fun play() {
+        while (true){
+
+        }
+    }
+
+    init {
+        println("Welcome, adventurer")
+        player.castFireBall(5)
+    }
+    private fun printPlayerStatus(player:Player) {
+        println(
+            "光環顏色：${player.auraColor()}" + "       走運嗎？" +
+                    "${if (player.isBlessed) "是的" else "否"}"
+        )
+        println("${player.name}${player.formaHealthStatus()}")
+    }
 }
