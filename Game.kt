6187ecf03988949cef14dea3632c38bcc1fa75d6
wl <kcwl558106@gmail.com>
2021-06-29 20:11:25 +0800

    c15-挑戰練習

--- Game.kt
+++ Game.kt
@@ -1,6 +1,11 @@
 import java.lang.IllegalStateException
 
 /*c15-20*/
+/*挑戰練習
+15.9: 實作 quit命令(Game.kt)
+15.10 挑戰練習:魔力地圖
+5.11  挑戰練習:搖鈴
+*/
 fun main() {
 //呼叫 Player 類別的主建構函數，產生一個類別實體
 //    var player = Player("wl")
@@ -23,15 +28,16 @@ fun main() {
 //    println("${player.name}${player.formaHealthStatus()}")
 //}
 object  Game{
+    final var isQuit =false
     private var player = Player("wl")
-    private var currentRoom: Room = TownSquare()
+    var currentRoom: Room = TownSquare()
     private  var worldMap = listOf(
         listOf(currentRoom,Room("Tavern"),Room("Back Room")),
         listOf(Room("Long Corridor"),Room("Generic Room")),
 
     )
     fun play() {
-        while (true){
+        while (!isQuit){
             println(currentRoom.description())
             println(currentRoom.load())
             player.auraColor()
@@ -58,10 +64,39 @@ object  Game{
         val command = input.split(" ")[0]
         val argument = input.split(" ").getOrElse(1,{""})
         fun processCommand() = when (command.toLowerCase()){
+            "quit" -> checkQuit()
+            "exit" -> checkQuit()
+            "map" -> showMap()
+            "ring" -> currentRoom.ringBell()
             "move" -> move(argument)
             else -> commandNotFound()
         }
         private  fun commandNotFound() ="I'm not quite sure what you're trying to do!"
+    }
+
+
+    private fun showMap(){
+        val  x= player.currentPosition.x
+        val  y =player.currentPosition.y
+        //println("x=$x,y=$y")
+        if(y==0){
+            if(x==0) println("XOO")
+            if(x==1) println("OXO")
+            if(x==2) println("OOX")
+            println("OO")
+        }
+        else{
+            println("OOO")
+            if(x==0) println("XO")
+            if(x==1) println("OX")
+        }
+
+
+    }
+
+    private  fun checkQuit(){
+        isQuit=true;
+        println("感謝你，再見！")
     }
     private  fun move(directionInput: String) =
         try {
--- Room.kt
+++ Room.kt
@@ -5,11 +5,11 @@ open class Room (val name:String){
     fun description() = "Room:$name\n" + "Danger level : $dangerLevel"
 
     open fun load() = "這裡沒什麼可看的..."
+    open fun ringBell() = "This is room bell"
 }
 open class TownSquare : Room("Town Square"){
     override val dangerLevel = super.dangerLevel -3
     private  var bellSound ="GWONG"
     final override fun load() = "The villagers rally and cheer as you enter！"
-    private  fun  ringBell() = "The bell tower announces your arrival. $bellSound"
-
+    final override fun ringBell() = "The bell tower announces your arrival. $bellSound"
 }
